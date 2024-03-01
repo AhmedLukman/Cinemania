8bd6523e57fb6c1a7fdf29d2b13c90cc0efa0223
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-01 16:39:30 +0300

    chore: Add constants

---
+++ src/lib/constants.ts
@@ -0,0 +1,96 @@
+export const POPULAR_MOVIES_OPTIONS = {
+  method: "GET",
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyZmM1NzhlZmRjMTUxMWQzNmRhNzQ5OTFmM2MxY2JlOSIsInN1YiI6IjY1ZGRlY2JlYzkyYzVkMDE2MzQ3NWY0MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.DqTP9erV2P9APWWUGBDG7N1jTx6Q26q9dADAy4aU1vY",
+  },
+};
+
+export const BASE_URL = "http://image.tmdb.org/t/p/";
+
+export const IMAGE_SIZE = "original";
+
+export const getGenreNameById = (genreId: number) => {
+  const genre = GENRES.find((genre) => genre.id === genreId);
+  return genre ? genre.name : "Unknown"; // If genre is found, return its name, otherwise return "Unknown"
+}
+
+export const GENRES = [
+  {
+    id: 28,
+    name: "Action",
+  },
+  {
+    id: 12,
+    name: "Adventure",
+  },
+  {
+    id: 16,
+    name: "Animation",
+  },
+  {
+    id: 35,
+    name: "Comedy",
+  },
+  {
+    id: 80,
+    name: "Crime",
+  },
+  {
+    id: 99,
+    name: "Documentary",
+  },
+  {
+    id: 18,
+    name: "Drama",
+  },
+  {
+    id: 10751,
+    name: "Family",
+  },
+  {
+    id: 14,
+    name: "Fantasy",
+  },
+  {
+    id: 36,
+    name: "History",
+  },
+  {
+    id: 27,
+    name: "Horror",
+  },
+  {
+    id: 10402,
+    name: "Music",
+  },
+  {
+    id: 9648,
+    name: "Mystery",
+  },
+  {
+    id: 10749,
+    name: "Romance",
+  },
+  {
+    id: 878,
+    name: "Science Fiction",
+  },
+  {
+    id: 10770,
+    name: "TV Movie",
+  },
+  {
+    id: 53,
+    name: "Thriller",
+  },
+  {
+    id: 10752,
+    name: "War",
+  },
+  {
+    id: 37,
+    name: "Western",
+  },
+];
