8d4278528f1e4e32260c9f2fc73e7bf7d8be4d2c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-01 19:52:16 +0300

    feat: Implement Backdrop component

---
+++ src/components/ui/Backdrop.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Backdrop = () => {
+  return (
+    <div className="absolute inset-0 -z-30 bg-gradient-to-b from-black/40 to-black" />
+  );
+}
+
+export default Backdrop
