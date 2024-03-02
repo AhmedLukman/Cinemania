5c5265a7ed84f740d139683f63c421af3213cf35
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-02 13:26:12 +0300

    chore: Place react slick style imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,9 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./Providers";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
