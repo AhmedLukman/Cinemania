8afd838ff5b43930725f8cd1a7f31e13e1fe22f7
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-04 17:08:01 +0300

    feat: Implement tv show category page

---
+++ src/app/(root)/tv/category/[id]/page.tsx
@@ -0,0 +1,67 @@
+import { IMAGE_URL, TVShowsUrl } from "@/lib/constants";
+import { fetchMovies } from "@/lib/utils";
+import { Card } from "@nextui-org/react";
+import Image from "next/image";
+import React from "react";
+
+const TVCategoryPage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const url =
+    id === "trending"
+      ? TVShowsUrl.Trending + "/week"
+      : id === "upcoming"
+      ? TVShowsUrl.Upcoming
+      : TVShowsUrl.TopRated;
+
+  let allTVShows: TTVShow[] = [];
+
+  for (let page = 1; page <= 3; page++) {
+    const moviesResponse = (await fetchMovies(
+      `${url}?language=en-US&page=${page}`
+    )) as TMediaResponse<TTVShow>;
+
+    allTVShows = [...allTVShows, ...moviesResponse.results];
+  }
+
+  return (
+    <div className="p-14">
+      <h2 className="text-3xl font-serif text-white my-10">
+        {id.startsWith("top")
+          ? id
+              .split("-")
+              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+              .join(" ")
+          : id.charAt(0).toUpperCase() + id.slice(1)}{" "}
+        TV Shows
+      </h2>
+      <div className="grid grid-cols-4 gap-8">
+        {allTVShows?.map((tvShow) => (
+          <Card
+            key={tvShow.id}
+            radius="lg"
+            className="border-none cursor-pointer group h-72 md:h-[400px] shadow-sm shadow-gray-500"
+          >
+            <Image
+              alt={tvShow.original_name}
+              className="object-cover group-hover:scale-110 transition duration-300"
+              fill
+              src={`${IMAGE_URL}${tvShow.poster_path}`}
+            />
+
+            <div className="w-full h-full bg-black opacity-0 group-hover:opacity-75 transition duration-300 z-20"></div>
+            <div className="absolute bottom-2 w-full md:group-hover:bottom-40 z-40 transition-all duration-500 flex items-center justify-center">
+              <h4 className="text-center mx-6 rounded-lg md:group-hover:border max-w-sm text-white text-lg bg-black/80 py-2 px-4">
+                {tvShow.original_name}
+              </h4>
+            </div>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default TVCategoryPage;
