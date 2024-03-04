51247ca5583c84dcf140ccd33c20515420ff487a
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-04 17:45:26 +0300

    style: Change background

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="h-screen bg-purple-50 w-screen flex items-center justify-center">
+    <div className="h-screen bg-gradient-to-b from-[#141414] to-black w-screen flex items-center justify-center">
       <CircularProgress color="secondary" size="lg" aria-label="Loading..." />
     </div>
   );
