d6df820db1e52e9e81f3176ba92dfab6d83f9db7
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-04 17:10:54 +0300

    feat: Implement routing on press

--- src/components/ui/MovieCategory.tsx
+++ src/components/ui/MovieCategory.tsx
@@ -1,6 +1,9 @@
+'use client'
+
 import React from "react";
 import MovieCategorySlider from "./MovieCategorySlider";
 import { Button } from "@nextui-org/react";
+import { usePathname, useRouter } from "next/navigation";
 
 const MovieCategory = ({
   movies,
@@ -9,13 +12,16 @@ const MovieCategory = ({
   movies: TMovie[] | TTVShow[];
   heading: string
 }) => {
+  const pathname = usePathname()
+  const router = useRouter()
+  const link = heading === 'Trending' ? 'trending' : heading === 'Top Rated' ? 'top-rated' : heading === 'Upcoming' ? 'upcoming' : ''
   return (
     <section>
       <div className="flex p-5 md:py-10 md:px-20 justify-between">
         <h3 className="text-white text-2xl md:text-3xl font-serif font-bold">
           {heading}
         </h3>
-        <Button className="text-white" variant="bordered">View more</Button>
+        <Button onPress={() => router.push(`${pathname}/category/${link}`)} className="text-white" variant="bordered">View more</Button>
       </div>
       <MovieCategorySlider movies={movies} />
     </section>
