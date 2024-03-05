20300135ab4d1af7e04adbda956049ee72549b96
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-05 15:19:23 +0300

    fix: Loading spinner style

--- src/app/(root)/loading.tsx
+++ src/app/(root)/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <div className="h-screen bg-gradient-to-b from-[#141414] to-black w-screen flex items-center justify-center">
+    <div className="absolute inset-0 flex justify-center items-center">
       <CircularProgress color="secondary" size="lg" aria-label="Loading..." />
     </div>
   );
