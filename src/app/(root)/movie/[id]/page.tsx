7ae2ca756a2f39807f482bddf0e16af23ccd98c8
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-09 20:14:58 +0300

    refactor: Client to server component

--- src/app/(root)/movie/[id]/page.tsx
+++ src/app/(root)/movie/[id]/page.tsx
@@ -1,23 +1,18 @@
-"use client";
-
 import PosterContainer from "@/components/ui/PosterContainer";
 import { MoviesUrl } from "@/lib/constants";
 import { fetchMovies } from "@/lib/utils";
 import React, { useEffect, useState } from "react";
 
-const SingleMoviePage = ({ params: { id } }: { params: { id: string } }) => {
-  const [data, setData] = useState<TMovieDetailsResponse>();
-  useEffect(() => {
-    const fetchData = async () => {
-      const popularMovies = (await fetchMovies(
-        MoviesUrl.Origin + id.toString() + "?language=en-US"
-      )) as TMovieDetailsResponse;
-      setData(popularMovies);
-    };
+const SingleMoviePage = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const movie = (await fetchMovies(
+    MoviesUrl.Origin + id.toString() + "?language=en-US"
+  )) as TMovieDetailsResponse;
 
-    fetchData();
-  }, [id]);
-  return <PosterContainer {...data!} />;
+  return <PosterContainer {...movie} />;
 };
 
 export default SingleMoviePage;
