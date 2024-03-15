bbf75395b68c1ef79f7fda7b894963f85096737c
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-15 23:13:07 +0300

    chore: Put logo in icons folder

--- src/components/ui/Logo.tsx
+++ src/components/ui/Logo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const Logo = () => {
   return (
     <Image
-      src={"/assets/images/cinemania-logo.png"}
+      src={"/assets/icons/cinemania-logo.png"}
       alt="Cinemania logo"
       width={100}
       height={100}
