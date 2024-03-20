89447ce4fc547655389dd460182add2965eaa1ab
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-20 16:57:36 +0300

    fix: Duplicate media card on small prop size

--- src/components/ui/MovieCategorySlider.tsx
+++ src/components/ui/MovieCategorySlider.tsx
@@ -2,47 +2,44 @@ import React from "react";
 import Slider from "react-slick";
 import MediaCard from "./MediaCard";
 
-const settings = {
-  infinite: true,
-  slidesToShow: 4,
-  slidesToScroll: 4,
-  draggable: false,
-  autoplay: true,
-  autoplaySpeed: 5000,
-};
-
 const MovieCategorySlider = ({
   movies,
 }: {
   movies: TMovie[] | TTVShow[] | TCrew[] | TCast[];
 }) => {
+  const length = movies.length
+  const settings = {
+    infinite: true,
+    slidesToShow: 4, // Show 4 slides at a time 
+    slidesToScroll: movies.length < 4 ? 1 : 4, 
+    draggable: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 3, // Show 3 slides at a time for smaller screens
+          slidesToScroll: movies.length < 3 ? 1 : 3,
+        },
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 2, // Show 2 slides at a time for even smaller screens
+          slidesToScroll: movies.length < 2 ? 1 : 2,
+          arrows: false,
+        },
+      },
+    ],
+  };
+
   return (
     <div className="slider-container md:px-10">
-      <Slider
-        lazyLoad="progressive"
-        responsive={[
-          {
-            breakpoint: 800, // This means at less than 640px screen width
-            settings: {
-              slidesToShow: 3, // Show only 2 slides
-              slidesToScroll: 3,
-            },
-          },
-          {
-            breakpoint: 640, // This means at less than 640px screen width
-            settings: {
-              slidesToShow: 2, // Show only 2 slides
-              slidesToScroll: 2,
-              arrows: false,
-            },
-          },
-        ]}
-        {...settings}
-      >
-        {movies.map((movie) => {
-          if ("profile_path" in movie) console.log(movie.profile_path);
-          return <MediaCard key={movie.id} {...movie} />;
-        })}
+      <Slider {...settings}>
+        {movies.map((movie) => (
+          <MediaCard key={movie.id} {...movie} />
+        ))}
       </Slider>
     </div>
   );
