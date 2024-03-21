8f8de0f8b2d8c6a0457b08672bb8dd82173d0754
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-21 04:16:59 +0300

    feat: Add similar and recommended

--- src/app/(root)/movie/[id]/page.tsx
+++ src/app/(root)/movie/[id]/page.tsx
@@ -20,21 +20,37 @@ const SingleMoviePage = async ({
     MoviesUrl.Origin + id + "/credits?language=en-US"
   )) as TMediaCreditsResponse;
 
+  const { results: similarMovies } = (await fetchMedia(
+    MoviesUrl.Origin + id + "/similar?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+
+  const { results: recommendedMovies } = (await fetchMedia(
+    MoviesUrl.Origin + id + "/recommendations?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+
   return (
     <>
       <PosterContainer {...movie} />
       {credits.cast.length !== 0 ? (
-        <MovieCategory path="/people" heading="Cast" movies={credits.cast} />
+        <MovieCategory
+          path={`/movie/${id}/people/cast`}
+          heading="Cast"
+          movies={credits.cast}
+        />
       ) : (
         <div className="p-5 md:py-10 md:px-20 space-y-5">
           <h3 className="text-white text-2xl md:text-3xl font-serif font-bold">
-            Crew
+            Cast
           </h3>
           <p className="text-red-500">No Cast data available</p>
         </div>
       )}
       {credits.crew.length !== 0 ? (
-        <MovieCategory path="/people" heading="Crew" movies={credits.crew} />
+        <MovieCategory
+          path={`/movie/${id}/people/crew`}
+          heading="Crew"
+          movies={credits.crew}
+        />
       ) : (
         <div className="p-5 md:py-10 md:px-20 space-y-5">
           <h3 className="text-white text-2xl md:text-3xl font-serif font-bold">
@@ -43,6 +59,16 @@ const SingleMoviePage = async ({
           <p className="text-red-500">No Crew data available</p>
         </div>
       )}
+      <MovieCategory
+        path={`/${id}/recommendations`}
+        heading="Recommended"
+        movies={recommendedMovies}
+      />
+      <MovieCategory
+        path={`/${id}/similar`}
+        heading="Similar"
+        movies={similarMovies}
+      />
     </>
   );
 };
--- src/app/(root)/tv/[id]/page.tsx
+++ src/app/(root)/tv/[id]/page.tsx
@@ -20,11 +20,23 @@ const SingleTVShowPage = async ({
     TVShowsUrl.Origin + id + "/credits?language=en-US"
   )) as TMediaCreditsResponse;
 
+  const { results: similarTV } = (await fetchMedia(
+    TVShowsUrl.Origin + id + "/similar?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+
+  const { results: recommendedTV } = (await fetchMedia(
+    TVShowsUrl.Origin + id + "/recommendations?language=en-US&page=1"
+  )) as TMediaResponse<TMovie>;
+
   return (
     <>
       <PosterContainer {...tv} />
       {credits.cast.length !== 0 ? (
-        <MovieCategory path="/people" heading="Cast" movies={credits.cast} />
+        <MovieCategory
+          path={`/tv/${id}/people/cast`}
+          heading="Cast"
+          movies={credits.cast}
+        />
       ) : (
         <div className="p-5 md:py-10 md:px-20 space-y-5">
           <h3 className="text-white text-2xl md:text-3xl font-serif font-bold">
@@ -34,7 +46,11 @@ const SingleTVShowPage = async ({
         </div>
       )}
       {credits.crew.length !== 0 ? (
-        <MovieCategory path="/people" heading="Crew" movies={credits.crew} />
+        <MovieCategory
+          path={`/tv/${id}/people/crew`}
+          heading="Crew"
+          movies={credits.crew}
+        />
       ) : (
         <div className="p-5 md:py-10 md:px-20 space-y-5">
           <h3 className="text-white text-2xl md:text-3xl font-serif font-bold">
@@ -43,6 +59,16 @@ const SingleTVShowPage = async ({
           <p className="text-red-500">No Crew data available</p>
         </div>
       )}
+      <MovieCategory
+        path={`/${id}/recommendations`}
+        heading="Recommended"
+        movies={recommendedTV}
+      />
+      <MovieCategory
+        path={`/${id}/similar`}
+        heading="Similar"
+        movies={similarTV}
+      />
     </>
   );
 };
