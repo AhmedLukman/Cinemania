cd5dc3dbb6c5a3c7818e549ab4023d39c2a21ad3
Ahmed Lukman <itsahmedlukman@gmail.com>
2024-03-21 20:26:47 +0300

    refactor: Buzz

--- src/components/ui/AllCategoryMedia.tsx
+++ src/components/ui/AllCategoryMedia.tsx
@@ -4,22 +4,25 @@ import MediaCard from "./MediaCard";
 const AllCategoryMedia = ({
   id,
   media,
-  heading
+  heading,
 }: {
-  id: string;
-  media: TMovie[] | TTVShow[];
-  heading: string
+  id?: string;
+  media: TMovie[] | TTVShow[] | TCast[] | TCrew[];
+  heading: string;
 }) => {
+  const buzz = id
+    ? id?.startsWith("top")
+      ? id
+          .split("-")
+          .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+          .join(" ")
+      : id.charAt(0).toUpperCase() + id.slice(1)
+    : "";
+
   return (
     <div className="md:p-14 pt-10">
       <h2 className="text-3xl font-serif text-white my-10 mx-2 md:mx-4">
-        {id.startsWith("top")
-          ? id
-              .split("-")
-              .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
-              .join(" ")
-          : id.charAt(0).toUpperCase() + id.slice(1)}{" "}
-        {heading}
+        {buzz} {heading}s
       </h2>
       <div className="grid grid-cols-2 md:grid-cols-4 gap-y-8">
         {media?.map((media) => (
