20a7a9e3e8aef0bb58842bbbf4661e8d346c11f6
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-27 00:28:32 +0300

    feat: Add PopularMovieSliderContent component for displaying popular movies

---
+++ src/components/features/PopularMovieSliderContent.tsx
@@ -0,0 +1,66 @@
+import { TmdbBackdropSizes, TmdbPosterSizes } from "@/lib/constants";
+import type { Movie } from "@/lib/types";
+import Backdrop from "../ui/Backdrop";
+import ImageWithBlur from "../ui/ImageWithBlur";
+import MovieDetails from "./MovieDetails";
+
+type PopularMovieSliderContentProps = {
+  popularMovie: Movie;
+  isFirstMovie: boolean;
+};
+
+const PopularMovieSliderContent = async ({
+  popularMovie: {
+    title,
+    backdrop_path,
+    poster_path,
+    id,
+    overview,
+    release_date,
+    vote_average,
+    genre_ids,
+  },
+  isFirstMovie,
+}: PopularMovieSliderContentProps) => {
+  return (
+    <div className="h-screen relative focus:outline-none">
+      {/* Full Screen Image */}
+      <ImageWithBlur
+        className="object-cover object-center -z-20"
+        alt={`${title} backdrop`}
+        sizes="100vw"
+        priority={isFirstMovie}
+        path={backdrop_path}
+        imageSize={TmdbBackdropSizes.ORIGINAL}
+        blurImageSize={TmdbBackdropSizes.W300}
+      />
+
+      <Backdrop />
+
+      <div className="h-full md:gap-5 lg:flex px-5 md:px-12 lg:px-16 xl:px-20 2xl:px-28 text-white">
+        <MovieDetails
+          genreIds={genre_ids}
+          id={id}
+          overview={overview}
+          releaseDate={release_date}
+          title={title}
+          voteAverage={vote_average}
+        />
+        {/* Side Image */}
+        <aside className="mt-10 hidden lg:block w-5/12 xl:w-4/12 h-[68svh] rounded-xl relative">
+          <ImageWithBlur
+            className=" object-cover object-center"
+            path={poster_path}
+            alt={`${title} poster`}
+            blurImageSize={TmdbPosterSizes.W92}
+            imageSize={TmdbPosterSizes.W780}
+            priority={isFirstMovie}
+            sizes="(min-width: 1280px) 33vw, (min-width: 1024px) 41.666vw, 0vw"
+          />
+        </aside>
+      </div>
+    </div>
+  );
+};
+
+export default PopularMovieSliderContent;
