0b7bedb2a355b863191c320dae23e64226de712f
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-26 22:48:13 +0300

    feat: Add image configuration for TMDB

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "image.tmdb.org",
+        pathname: "/t/p/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
