c68a29b5a4e30faa4ea51287b4b60ff71b6e8aa4
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-26 22:49:21 +0300

    feat: Add TMDB image and genre endpoint constants

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,5 +1,7 @@
 export const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 
+export const TMDB_IMAGE_BASE_URL = "https://image.tmdb.org/t/p";
+
 export const TmdbApiMovieEndpoints = {
   Popular: "/movie/popular",
   NowPlaying: "/movie/now_playing",
@@ -11,3 +13,34 @@ export const TmdbApiMovieEndpoints = {
 
 export type TmdbApiMovieEndpointsType =
   (typeof TmdbApiMovieEndpoints)[keyof typeof TmdbApiMovieEndpoints];
+
+export const TmdbBackdropSizes = {
+  W300: "/w300",
+  W780: "/w780",
+  W1280: "/w1280",
+  ORIGINAL: "/original",
+} as const;
+
+export type TmdbBackdropSizesType =
+  (typeof TmdbBackdropSizes)[keyof typeof TmdbBackdropSizes];
+
+export const TmdbPosterSizes = {
+  W92: "/w92",
+  W154: "/w154",
+  W185: "/w185",
+  W342: "/w342",
+  W500: "/w500",
+  W780: "/w780",
+  ORIGINAL: "/original",
+} as const;
+
+export type TmdbPosterSizesType =
+  (typeof TmdbPosterSizes)[keyof typeof TmdbPosterSizes];
+
+export const TmdbApiGenreEndpoints = {
+  MovieGenres: "/genre/movie/list",
+  TvGenres: "/genre/tv/list",
+} as const;
+
+export type TmdbApiGenreEndpointsType =
+  (typeof TmdbApiGenreEndpoints)[keyof typeof TmdbApiGenreEndpoints];
