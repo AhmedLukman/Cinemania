bfa31e49c0d4d2776c67a147a5f1a1547c825b9e
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-27 21:27:21 +0300

    feat: Add PopularMoviesSmallSliderContent component for displaying popular movies with hover effect

---
+++ src/components/features/PopularMoviesSmallSliderContent.tsx
@@ -0,0 +1,30 @@
+import { TmdbBackdropSizes } from "@/lib/constants";
+import type { Movie } from "@/lib/types";
+import ImageWithBlur from "../ui/ImageWithBlur";
+import HoverCard from "./HoverCard";
+
+type PopularMoviesSmallSliderContentProps = {
+  popularMovie: Movie;
+  index: number;
+};
+
+const PopularMoviesSmallSliderContent = ({
+  popularMovie: { id, backdrop_path, title },
+  index,
+}: PopularMoviesSmallSliderContentProps) => {
+  return (
+    <HoverCard key={id} index={index} title={title}>
+      <ImageWithBlur
+        sizes="(max-width: 540px) calc(50vw - 1rem), (max-width: 800px) calc(33vw - 1rem), calc(20vw - 1rem)"
+        path={backdrop_path}
+        alt={`${title} thumbnail`}
+        priority={index < 5}
+        className="object-cover scale-[1.15]"
+        imageSize={TmdbBackdropSizes.W780}
+        blurImageSize={TmdbBackdropSizes.W300}
+      />
+    </HoverCard>
+  );
+};
+
+export default PopularMoviesSmallSliderContent;
