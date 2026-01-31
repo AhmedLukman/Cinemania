f23cc388e08506135ebaf01d3b38db4bd4071416
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-31 22:20:14 +0300

    refactor: Update background gradient class in Backdrop component

--- src/components/ui/Backdrop.tsx
+++ src/components/ui/Backdrop.tsx
@@ -1,6 +1,6 @@
 const Backdrop = () => {
   return (
-    <div className="absolute inset-0 bg-gradient-to-b from-black/40 to-black -z-10" />
+    <div className="absolute inset-0 bg-linear-to-b from-black/40 to-black -z-10" />
   );
 };
 
