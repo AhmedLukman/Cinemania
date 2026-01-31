66261be3b6d887f7cb558584b5d282c90aeec4d2
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-01-31 21:23:31 +0300

    refactor: Remove type exports from constants file

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -11,18 +11,12 @@ export const TmdbApiMovieEndpoints = {
   Latest: "/movie/latest",
 } as const;
 
-export type TmdbApiMovieEndpointsType =
-  (typeof TmdbApiMovieEndpoints)[keyof typeof TmdbApiMovieEndpoints];
-
 export const TmdbBackdropSizes = {
   W300: "/w300",
   W780: "/w780",
   W1280: "/w1280",
   ORIGINAL: "/original",
 } as const;
-
-export type TmdbBackdropSizesType =
-  (typeof TmdbBackdropSizes)[keyof typeof TmdbBackdropSizes];
 
 export const TmdbPosterSizes = {
   W92: "/w92",
@@ -34,16 +28,10 @@ export const TmdbPosterSizes = {
   ORIGINAL: "/original",
 } as const;
 
-export type TmdbPosterSizesType =
-  (typeof TmdbPosterSizes)[keyof typeof TmdbPosterSizes];
-
 export const TmdbApiGenreEndpoints = {
   MovieGenres: "/genre/movie/list",
   TvGenres: "/genre/tv/list",
 } as const;
-
-export type TmdbApiGenreEndpointsType =
-  (typeof TmdbApiGenreEndpoints)[keyof typeof TmdbApiGenreEndpoints];
 
 export const TmdbProfileSizes = {
   W45: "/w45",
@@ -52,18 +40,12 @@ export const TmdbProfileSizes = {
   ORIGINAL: "/original",
 } as const;
 
-export type TmdbProfileSizesType =
-  (typeof TmdbProfileSizes)[keyof typeof TmdbProfileSizes];
-
 export const TmdbStillSizes = {
   W92: "/w92",
   W185: "/w185",
   W300: "/w300",
   ORIGINAL: "/original",
 } as const;
-
-export type TmdbStillSizesType =
-  (typeof TmdbStillSizes)[keyof typeof TmdbStillSizes];
 
 export const TmdbLogoSizes = {
   W45: "/w45",
@@ -74,6 +56,3 @@ export const TmdbLogoSizes = {
   W500: "/w500",
   ORIGINAL: "/original",
 } as const;
-
-export type TmdbLogoSizesType =
-  (typeof TmdbLogoSizes)[keyof typeof TmdbLogoSizes];
