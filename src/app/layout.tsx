797985db1a64b78fc44dd257a0a34448e07bf0a7
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-02-01 12:07:39 +0300

    style: Add body background gradient

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-linear-to-b from-[#151515] to-[#141414]`}
       >
         {children}
       </body>
