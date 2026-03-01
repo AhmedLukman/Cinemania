d2d3fe4baa2638a1af15be4bfbc8f50460b44882
Ahmed Lukman <itsahmedlukman@gmail.com>
2026-03-01 12:21:54 +0300

    refactor: Enhance BorderButton component to allow returning button

--- src/components/ui/BorderButton.tsx
+++ src/components/ui/BorderButton.tsx
@@ -1,25 +1,33 @@
-import { buttonVariants } from "@heroui/react";
+import { Button, type ButtonProps, buttonVariants } from "@heroui/react";
 import Link from "next/link";
 import RippleUI from "./RippleUI";
 
 type BorderButtonProps = {
-  href: string;
+  href?: string;
   children: React.ReactNode;
-};
+} & ButtonProps;
 
-const BorderButton = ({ href, children }: BorderButtonProps) => {
+const BorderButton = ({ href, children, ...props }: BorderButtonProps) => {
+  const className = buttonVariants({
+    variant: "outline",
+    className:
+      "text-white rounded-xl border-2 transition-all duration-300 2xl:p-5 bg-transparent relative hover:opacity-75",
+  });
+
+  if (href) {
+    return (
+      <Link href={href} className={className}>
+        <RippleUI />
+        {children}
+      </Link>
+    );
+  }
+
   return (
-    <Link
-      href={href}
-      className={buttonVariants({
-        variant: "outline",
-        className:
-          "text-white rounded-xl border-2 transition-all duration-300 2xl:p-5 hover:bg-transparent relative hover:opacity-75",
-      })}
-    >
+    <Button {...props} className={className}>
       <RippleUI />
       {children}
-    </Link>
+    </Button>
   );
 };
 
